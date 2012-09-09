66f47681453f4694f0029f96e10e4238ad74c6f1
felix <felix.guendling@gmx.de>
2012-09-09 02:14:27 +0200

    more debug output for login

--- base.lua
+++ base.lua
@@ -1,14 +1,14 @@
 function login(username, password)
-	local param = {}
-	param["username"] = username
-	param["password"] = password
+  m_log("getting start page")
 	local response = m_request_path("/")
-
 	if string.find(response, 'action="/logout/"', 0, true) then
 		m_log("already logged in")
 		return true
 	end
 
+	local param = {}
+	param["username"] = username
+	param["password"] = password
 	m_log("logging in")
 	response = m_submit_form(response, "//input[@name = 'submitForm']", param)
 
